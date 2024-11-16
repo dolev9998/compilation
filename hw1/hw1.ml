216758bfd1aab4665aa30c6c09da4939446290a0
dolev9998 <dolevdav@bgu.ac.il>
2024-11-16 21:46:07 +0200

    a

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -47,6 +47,13 @@ let int_of_digit_char =
 let nt_digit_0_9 = 
   pack (range '0' '9') (int_of_digit_char)
 
+(*
+let nt_optional_is_not_divided =
+  let nt1 = pack (char '/') (fun _ -> false) in
+  let nt1 = maybeify nt1 true in
+  nt1;;
+  
+*)
 
 let nt_int = 
   let nt1 = pack (plus nt_digit_0_9)
@@ -76,10 +83,14 @@ let make_nt_paren ch_left ch_right nt=
 let nt_var = 
   let nt1 = range_ci 'a' 'z' in
   let nt2 = range '0' '9' in
-  let nt1 = caten nt1 (star (disj nt1 nt2)) in
+  let nt3 = const (fun ch -> ch = '$' || ch = '_') in
+  let nt1 = caten nt1 (star (disj (disj nt1 nt2) nt3)) in
   let nt1 = pack nt1 (fun (ch1, chs) -> string_of_list (ch1 :: chs)) in
   let nt1 = pack nt1 (fun name -> Var name) in
   nt1;;
+
+let nt_mod = 
+  caten (const (fun ch -> ch = 'm' )) (caten  (const (fun ch -> ch = 'o' )) (const (fun ch -> ch = 'd' ))) 
 
 let rec nt_expr str = nt_expr_0 str
 and nt_expr_0 str =
@@ -91,23 +102,29 @@ and nt_expr_0 str =
   let nt1 = make_nt_spaced_out nt1 in
   nt1 str
 and nt_expr_1 str =
-    let nt1 = pack (char '*') (fun _ -> Add) in
-    let nt2 = pack (char '/') (fun _ -> Sub) in
-    let nt1 = disj nt1 nt2 in
-    let nt1 = star (caten nt1 nt_expr_1) in
-    let nt1 = pack (caten nt_expr_1 nt1) (fun (expr2, binop_expr2) -> List.fold_left (fun expr2 (binop, expr2') -> BinOp (binop, expr2, expr2')) expr2 binop_expr2) in
+    let nt1 = pack (char '*') (fun _ -> Mul) in
+    let nt2 = pack (char '/') (fun _ -> Div) in
+    let nt3 = pack nt_mod (fun _ -> Mod) in
+    let nt1 = disj nt1 (disj nt2 nt3) in
+    let nt1 = star (caten nt1 nt_expr_2) in
+    let nt1 = pack (caten nt_expr_2 nt1) (fun (expr2, binop_expr2) -> List.fold_left (fun expr2 (binop, expr2') -> BinOp (binop, expr2, expr2')) expr2 binop_expr2) in
     let nt1 = make_nt_spaced_out nt1 in
     nt1 str
 and nt_expr_2 str =
+    let nt1 = pack (char '^') (fun _ -> Pow) in
+    let nt1 = star (caten nt1 nt_expr_3) in
+    let nt1 = pack (caten nt_expr_3 nt1) (fun (expr3, binop_expr3) -> List.fold_right (fun expr3 (binop, expr3') -> BinOp (binop, expr3, expr3')) expr3 binop_expr3) in
+    let nt1 = make_nt_spaced_out nt1 in
+    nt1 str
+
+and nt_expr_3 str =
   let nt1 = pack nt_int (fun num -> Num num) in
   let nt1 = disj nt1 nt_var in
   let nt1 = disj nt1 nt_paren in
   let nt1 = make_nt_spaced_out nt1 in
   nt1 str
 and nt_paren str =
-  disj_list [make_nt_paren '(' ')' nt_expr;
-  make_nt_paren '[' ']' nt_expr;
-  make_nt_paren '{' '}' nt_expr] str
+  (make_nt_paren '(' ')')  str
 ;;
 
 end;; (* module InfixParser *)
